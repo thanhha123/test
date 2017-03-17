27b8b96854698a8240528ec0ae891547ca752409
Truong Thanh Ha <thienhabka@gmail.com>
2017-03-17 12:04:12 +0700

    Update fio.sh

--- fio.sh
+++ fio.sh
@@ -1,4 +1,4 @@
-fio --directory=/root/ --direct=1 --rw=$1 --bs=$2 --numjobs=8 --iodepth=64 --runtime=300 --group_reporting --name=testIOPslan1 --output=$3
+fio --directory=/root/ --direct=1 --rw=$1 --bs=$2 --size=10M --numjobs=128 --iodepth=64 --runtime=600 --group_reporting --name=testIOPslan1 --output=$3
 echo "##################xoa file###########"
 rm /root/testIOP*
 echo "################Xoa cache#######"
